f2b85430cce393218cee11f8b73a24f3f9ab853a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-09 12:10:24 +0530

    Create User.js

---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+   
+    phone:{
+      type: String,
+      required: true,
+    },
+    
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", UserSchema);

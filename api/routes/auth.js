42e8dc678af568326ba8b06a006866fff457e9fa
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-09 12:10:19 +0530

    Create auth.js

---
+++ api/routes/auth.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+const bcrypt = require("bcrypt");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+//register
+router.post("/register", async (req, res) => {
+
+  try {
+    //generate ne password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    
+    //create new user
+    const newUser = new User({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: hashedPassword,
+      phone: req.body.phone,
+      
+    });
+    console.log(newUser.phone)
+
+    //save user and return response
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).send("user not found");
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;

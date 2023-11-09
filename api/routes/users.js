a4c990297a6bd2705fb342fe62f9802ca11afebd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-09 12:10:18 +0530

    Create users.js

---
+++ api/routes/users.js
@@ -0,0 +1,78 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId == req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can update only your account");
+  }
+});
+//delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId == req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete({ _id: req.params.id });
+      res.status(200).json("Account has been deleted");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account");
+  }
+});
+//get user
+router.get("/", async (req, res) => {
+  const userId = req.query.userId;
+  const username = req.query.username;
+  try {
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+//update
+router.route("/:id").put(async(req, res) => {
+  let userId = req.params.id;
+  const {firstnamne,lastname,phone,email} = req.body;
+
+  const updateStudent = {
+      firstnamne,
+      lastname,
+      phone,
+      email,
+      
+  }
+
+  const update = await User.findByIdAndUpdate(userId, updateStudent)
+      .then(()=>{
+          res.status(200).send({status : "User Updated"});
+      })
+      .catch((err)=>{
+          res.status(500).send({status: "Error on update data"});
+      })
+})
+
+module.exports = router;

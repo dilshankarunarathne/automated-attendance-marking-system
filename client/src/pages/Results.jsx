ec4c3cac06aaaf715f7a9cebc94245d1aeaee942
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-09 20:02:10 +0530

    Update Results.jsx

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -1,1 +1,52 @@
+import { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import axios from 'axios';
 
+export default function Results() {
+  const { user } = useContext(AuthContext);
+  const [index, setIndex] = useState("");
+  const [resultsData, setResultsData] = useState([]);
+
+  async function searchByIndex(e) {
+    e.preventDefault();
+
+    try {
+      const response = await axios.get(`http://localhost:8070/results/index/${index}`);
+      setResultsData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="results">
+      <form onSubmit={searchByIndex}>
+        <label>Index:</label>
+        <input type="text" value={index} onChange={(e) => setIndex(e.target.value)} />
+        <button type="submit">Search by Index</button>
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>Index</th>
+            <th>Grade</th>
+            <th>Semester</th>
+            <th>Subject</th>
+            <th>Mark</th>
+          </tr>
+        </thead>
+        <tbody>
+          {resultsData.map((data) => (
+            <tr key={data._id}>
+              <td>{data.index}</td>
+              <td>{data.grade}</td>
+              <td>{data.semester}</td>
+              <td>{data.subject}</td>
+              <td>{data.mark}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}

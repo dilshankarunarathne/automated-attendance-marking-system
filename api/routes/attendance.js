d6c1fd0bbca1a645b52fea8a77b24613c5e1f187
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-09 20:21:07 +0530

    Update attendance.js

---
+++ api/routes/attendance.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const Attendance = require('../models/Attendance'); // Replace with your actual Attendance model path
+
+const router = express.Router();
+
+router.get('/date/:date', async (req, res) => {
+  const { date } = req.params;
+
+  try {
+    const attendance = await Attendance.find({ date });
+    res.json(attendance);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.get('/index/:index', async (req, res) => {
+  const { index } = req.params;
+
+  try {
+    const attendance = await Attendance.find({ index });
+    res.json(attendance);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+router.post('/mark/:fingerprint_data', async (req, res) => {
+  const { fingerprint_data } = req.params;
+
+  // Use fingerprint_data to find the student index number
+  // This part is not included in the code because it depends on how you're handling fingerprint data
+
+  const newAttendance = new Attendance({
+    index: /* student index number */,
+    date: new Date(),
+    timestamp: Date.now()
+  });
+
+  try {
+    const savedAttendance = await newAttendance.save();
+    res.json(savedAttendance);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;

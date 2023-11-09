d1a9b582e029443589026413e350090be7e0f13b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-09 19:14:01 +0530

    Update Attendance.jsx

--- client/src/pages/Attendance.jsx
+++ client/src/pages/Attendance.jsx
@@ -3,6 +3,7 @@ import './attendance.css'
 import  { useContext ,useState} from 'react'
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
 
 export default function Attendance() {
 

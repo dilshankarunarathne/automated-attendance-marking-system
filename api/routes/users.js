a5cb631f8d715522b085a473ecb75701e148ec71
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-19 20:35:30 +0530

    Update users.js

--- api/routes/users.js
+++ api/routes/users.js
@@ -43,33 +43,4 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-/*
-router.get("/", async (req, res) => {
-  const userId = req.query.userId;
-  const username = req.query.username;
-  try {
-    const user = userId
-      ? await User.findById(userId)
-      : await User.findOne({ username: username });
-    const { password, updatedAt, ...other } = user._doc;
-    res.status(200).json(other);
-  } catch (error) {
-    return res.status(500).json(error);
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  if (req.body.userId == req.params.id || req.body.isAdmin) {
-    try {
-      const user = await User.findByIdAndDelete({ _id: req.params.id });
-      res.status(200).json("Account has been deleted");
-    } catch (error) {
-      return res.status(500).json(error);
-    }
-  } else {
-    return res.status(403).json("You can delete only your account");
-  }
-});
-*/
-
 module.exports = router;
